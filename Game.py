6a30b04659ae53d03a8b672e5015bb7a4747dd8f
DARKSONIC <kevin-lehmann99@web.de>
2022-11-12 22:55:34 +0100

    Added Key events that change colors

--- Game.py
+++ Game.py
@@ -9,19 +9,28 @@ def main():
 
     GUI = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_LENGTH))
     pygame.display.set_caption("SpaceShooter")
-    run = True
+    running = True
+    GUI.fill((40, 40, 40))
+    pygame.display.update()
 
-    while run:
+    while running:
 
         for event in pygame.event.get():
+            print(event)
             if event.type == pygame.QUIT:
-                run = False
+                running = False
         
-        GUI.fill((40, 40, 40))
-        pygame.display.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    background = (0,0,0)
+                elif event.key == pygame.K_g:
+                    background = (255,255,255)
+
+                GUI.fill(background)
+                pygame.display.update()
 
     pygame.quit()
-
+    
 if __name__ == '__main__':
     main()
     pygame.quit()
