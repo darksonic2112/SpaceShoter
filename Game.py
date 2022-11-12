1fb92e61df280ea28c2a7c02ac8e3cb0313f761d
DARKSONIC <kevin-lehmann99@web.de>
2022-11-12 22:41:15 +0100

    created main function and display loop

--- Game.py
+++ Game.py
@@ -2,6 +2,26 @@ import pygame
 
 pygame.display.init()
 
-print(pygame.display.get_init())
+def main():
 
-pygame.display.quit()
+    WINDOW_WIDTH = 800
+    WINDOW_LENGTH = 1000
+
+    GUI = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_LENGTH))
+    pygame.display.set_caption("SpaceShooter")
+    run = True
+
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        GUI.fill((40, 40, 40))
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
+    pygame.quit()
