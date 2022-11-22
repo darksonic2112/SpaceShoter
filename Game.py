cff323258913c8a4ce8405f2b1afa03f67684871
DARKSONIC <kevin-lehmann99@web.de>
2022-11-22 21:47:19 +0100

    Created movable Object with velocity and stuff

--- Game.py
+++ Game.py
@@ -8,17 +8,17 @@ def main():
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 1000
 
-    SHIP_WIDTH = 50
-    SHIP_HEIGHT = 100
-    SHIP_START_POS_X = 325
-    SHIP_START_POS_Y = 850
+    SHIP_WIDTH = 25
+    SHIP_HEIGHT = 50
+    SHIP_POS_X = 330
+    SHIP_POS_Y = 850
+    VELOCITY = 0.3
 
     GUI = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("SpaceShooter")
     running = True
-    background = (40, 40, 40)
-    GUI.fill(background) # Dark grey
-    SHIP = pygame.draw.rect(GUI, (255,255,255), (SHIP_START_POS_X, SHIP_START_POS_Y, SHIP_WIDTH, SHIP_HEIGHT))
+    BACKGROUND = (40, 40, 40)
+    GUI.fill(BACKGROUND) # Dark grey
     pygame.display.update()
     mouse_position = pygame.mouse.get_pos()
     moving = False
@@ -29,13 +29,25 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-            elif event.type == MOUSEMOTION:
-                SHIP.move_ip(event.rel)
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and SHIP_POS_X > 0:
+                SHIP_POS_X -= VELOCITY
             
+        if keys[pygame.K_RIGHT] and SHIP_POS_X < (WINDOW_WIDTH - SHIP_WIDTH):
+                SHIP_POS_X += VELOCITY
 
+        if keys[pygame.K_UP] and SHIP_POS_Y > 0:
+                SHIP_POS_Y -= VELOCITY
+
+        if keys[pygame.K_DOWN] and SHIP_POS_Y < (WINDOW_HEIGHT - (SHIP_HEIGHT/2)):
+                SHIP_POS_Y += VELOCITY
        
-            pygame.display.update()
-
+        GUI.fill(BACKGROUND)
+        SHIP = pygame.draw.rect(GUI, (255,255,255), (SHIP_POS_X, SHIP_POS_Y, SHIP_WIDTH, SHIP_HEIGHT))
+        pygame.display.update()
+    
+    
     pygame.quit()
     
 if __name__ == '__main__':
---
+++ Window.py
@@ -0,0 +1,9 @@
+import pygame
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 1000
+
+BACKGROUND = (40, 40, 40)
+
+WINDOW = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW.fill(BACKGROUND) # Dark grey
