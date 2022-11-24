f6c4fe631c34c8e22372f660424ff259d30d7750
DARKSONIC <kevin-lehmann99@web.de>
2022-11-24 23:11:39 +0100

    Major changes (new version)

--- Game.py
+++ Game.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import *
 
-pygame.display.init()
+pygame.init()
+pygame.display.set_caption("SpaceShooter")
 
 def main():
 
@@ -23,7 +24,6 @@ def main():
     BACKGROUND = (40, 40, 40)
     GUI = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
     GUI.fill(BACKGROUND) # Dark grey
-    pygame.display.set_caption("SpaceShooter")
 
     clock = pygame.time.Clock()
 
@@ -37,7 +37,6 @@ def main():
                 running = False
 
         
-        PROJECTILE = pygame.draw.rect(GUI, (255,255,255), (PROJECTILE_POS_X, PROJECTILE_POS_Y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT))
         PROJECTILE_POS_Y += PROJECTILE_VELOCITY
 
 
@@ -54,9 +53,10 @@ def main():
 
         if keys[pygame.K_DOWN] and SHIP_POS_Y < (WINDOW_HEIGHT - (SHIP_HEIGHT/2)):
                 SHIP_POS_Y += SHIP_VELOCITY
-       
+
         GUI.fill(BACKGROUND)
         SHIP = pygame.draw.rect(GUI, (255,255,255), (SHIP_POS_X, SHIP_POS_Y, SHIP_WIDTH, SHIP_HEIGHT))
+        PROJECTILE = pygame.draw.rect(GUI, (255,255,255), (SHIP_POS_X, (SHIP_POS_Y + 1), PROJECTILE_WIDTH, PROJECTILE_HEIGHT))
         
         pygame.display.update()
     
--- Ship.py
+++ Ship.py
@@ -1,5 +1,12 @@
-SHIP_WIDTH = 25
-SHIP_HEIGHT = 50
-SHIP_POS_X = 330
-SHIP_POS_Y = 850
-VELOCITY = 0.3
+import pygame
+
+class Ship:
+    def __init__(self) -> None:
+        pass
+    SHIP_WIDTH = 25
+    SHIP_HEIGHT = 50
+    SHIP_POS_X = 330
+    SHIP_POS_Y = 850
+    VELOCITY = 0.3
+
+    SHIP = pygame.draw.rect(GUI, (255,255,255), (SHIP_POS_X, SHIP_POS_Y, SHIP_WIDTH, SHIP_HEIGHT))
