33b317d5796e9048d7e10357252183d721d3caf8
DARKSONIC <kevin-lehmann99@web.de>
2023-12-26 10:31:29 +0100

    Spaceship graphic is bound to hitbox now and can move, ship can now move slower by holding shift, window_width adjusted

--- Game.py
+++ Game.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-WINDOW_WIDTH = 800
+WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 1000
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
@@ -13,14 +13,14 @@ GREY = (25, 25, 25)
 BLACK = (0, 0, 0)
 SPACESHIP_GRAPHIC = pygame.image.load("Space_Ship.png")
 
-SHIP_WIDTH = 25
-SHIP_HEIGHT = 50
+SHIP_WIDTH = 16
+SHIP_HEIGHT = 20
 ship_pos_x = 330
 ship_pos_y = 850
-SHIP_VELOCITY = 1
+SHIP_VELOCITY = 1.75
 SHIP_COLOR = WHITE
 
-SPACESHIP = (ship_pos_x, ship_pos_y, SHIP_WIDTH, SHIP_HEIGHT)
+HITBOX = (ship_pos_x + 8, ship_pos_y + 10, SHIP_WIDTH, SHIP_HEIGHT)
 
 
 def ship_animation():
@@ -31,7 +31,7 @@ def ship_animation():
 def redraw_game_window():
     window.fill(GREY)
     pygame.draw.rect(window, SHIP_COLOR, (ship_pos_x, ship_pos_y, SHIP_WIDTH, SHIP_HEIGHT))
-    window.blit(SPACESHIP_GRAPHIC, (100, 100))
+    window.blit(SPACESHIP_GRAPHIC, (ship_pos_x, ship_pos_y))
     pygame.display.update()
 
 
@@ -46,20 +46,36 @@ while run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and ship_pos_x > 0:
-        ship_pos_x -= SHIP_VELOCITY
-        ship_animation()
+        if keys[pygame.K_LSHIFT]:
+            ship_pos_x -= SHIP_VELOCITY / 3
+            ship_animation()
+        else:
+            ship_pos_x -= SHIP_VELOCITY
+            ship_animation()
 
-    if keys[pygame.K_RIGHT] and ship_pos_x < (800 - SHIP_WIDTH):
-        ship_pos_x += SHIP_VELOCITY
-        ship_animation()
+    if keys[pygame.K_RIGHT] and ship_pos_x < (WINDOW_WIDTH - SHIP_WIDTH * 2):
+        if keys[pygame.K_LSHIFT]:
+            ship_pos_x += SHIP_VELOCITY / 3
+            ship_animation()
+        else:
+            ship_pos_x += SHIP_VELOCITY
+            ship_animation()
 
     if keys[pygame.K_UP] and ship_pos_y > 0:
-        ship_pos_y -= SHIP_VELOCITY
-        ship_animation()
+        if keys[pygame.K_LSHIFT]:
+            ship_pos_y -= SHIP_VELOCITY / 3
+            ship_animation()
+        else:
+            ship_pos_y -= SHIP_VELOCITY
+            ship_animation()
 
-    if keys[pygame.K_DOWN] and ship_pos_y < (1000 - (SHIP_HEIGHT / 2)):
-        ship_pos_y += SHIP_VELOCITY
-        ship_animation()
+    if keys[pygame.K_DOWN] and ship_pos_y < (1000 - 40):
+        if keys[pygame.K_LSHIFT]:
+            ship_pos_y += SHIP_VELOCITY / 3
+            ship_animation()
+        else:
+            ship_pos_y += SHIP_VELOCITY
+            ship_animation()
 
     redraw_game_window()
 
