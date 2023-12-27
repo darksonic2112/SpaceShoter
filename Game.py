2e8c762d7b0bdd80e904c318fbc6c55bc3ed9ac7
DARKSONIC <kevin-lehmann99@web.de>
2023-12-27 23:35:13 +0100

    Added graphic when spaceship goes down, not fully implemented yet

--- Game.py
+++ Game.py
@@ -11,7 +11,10 @@ clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
 GREY = (25, 25, 25)
 BLACK = (0, 0, 0)
+
 SPACESHIP_GRAPHIC = pygame.image.load("Space_Ship.png")
+SPACESHIP_GRAPHIC_STANDARD = pygame.image.load("Space_Ship.png")
+SPACESHIP_GRAPHIC_DOWN = pygame.image.load("Space_Ship_Backwards.png")
 
 SHIP_WIDTH = 16
 SHIP_HEIGHT = 20
@@ -23,12 +26,18 @@ SHIP_COLOR = WHITE
 HITBOX = (ship_pos_x + 8, ship_pos_y + 10, SHIP_WIDTH, SHIP_HEIGHT)
 
 
-def ship_animation():
+def ship_animation(direction="standard"):
     global SHIP_COLOR
     SHIP_COLOR = BLACK
+    match direction:
+        case "down":
+            SPACESHIP_GRAPHIC = SPACESHIP_GRAPHIC_DOWN
+        case "standard":
+            window.blit(SPACESHIP_GRAPHIC_STANDARD, (ship_pos_x, ship_pos_y))
 
 
 def redraw_game_window():
+    global SPACESHIP_GRAPHIC
     window.fill(GREY)
     pygame.draw.rect(window, SHIP_COLOR, (ship_pos_x, ship_pos_y, SHIP_WIDTH, SHIP_HEIGHT))
     window.blit(SPACESHIP_GRAPHIC, (ship_pos_x, ship_pos_y))
@@ -72,10 +81,10 @@ while run:
     if keys[pygame.K_DOWN] and ship_pos_y < (1000 - 40):
         if keys[pygame.K_LSHIFT]:
             ship_pos_y += SHIP_VELOCITY / 3
-            ship_animation()
+            ship_animation("down")
         else:
             ship_pos_y += SHIP_VELOCITY
-            ship_animation()
+            ship_animation("down")
 
     redraw_game_window()
 
