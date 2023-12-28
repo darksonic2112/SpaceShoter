99c01d0f9bc61fbf826b9a95fb8c616e5f516f5d
DARKSONIC <kevin-lehmann99@web.de>
2023-12-28 22:15:16 +0100

    Deleted everything with ship animation due to complexity with graphics.

--- Game.py
+++ Game.py
@@ -13,8 +13,6 @@ GREY = (25, 25, 25)
 BLACK = (0, 0, 0)
 
 SPACESHIP_GRAPHIC = pygame.image.load("Space_Ship.png")
-SPACESHIP_GRAPHIC_STANDARD = pygame.image.load("Space_Ship.png")
-SPACESHIP_GRAPHIC_DOWN = pygame.image.load("Space_Ship_Backwards.png")
 
 SHIP_WIDTH = 16
 SHIP_HEIGHT = 20
@@ -24,16 +22,6 @@ SHIP_VELOCITY = 1.75
 SHIP_COLOR = WHITE
 
 HITBOX = (ship_pos_x + 8, ship_pos_y + 10, SHIP_WIDTH, SHIP_HEIGHT)
-
-
-def ship_animation(direction="standard"):
-    global SHIP_COLOR
-    SHIP_COLOR = BLACK
-    match direction:
-        case "down":
-            SPACESHIP_GRAPHIC = SPACESHIP_GRAPHIC_DOWN
-        case "standard":
-            window.blit(SPACESHIP_GRAPHIC_STANDARD, (ship_pos_x, ship_pos_y))
 
 
 def redraw_game_window():
@@ -57,34 +45,26 @@ while run:
     if keys[pygame.K_LEFT] and ship_pos_x > 0:
         if keys[pygame.K_LSHIFT]:
             ship_pos_x -= SHIP_VELOCITY / 3
-            ship_animation()
         else:
             ship_pos_x -= SHIP_VELOCITY
-            ship_animation()
 
     if keys[pygame.K_RIGHT] and ship_pos_x < (WINDOW_WIDTH - SHIP_WIDTH * 2):
         if keys[pygame.K_LSHIFT]:
             ship_pos_x += SHIP_VELOCITY / 3
-            ship_animation()
         else:
             ship_pos_x += SHIP_VELOCITY
-            ship_animation()
 
     if keys[pygame.K_UP] and ship_pos_y > 0:
         if keys[pygame.K_LSHIFT]:
             ship_pos_y -= SHIP_VELOCITY / 3
-            ship_animation()
         else:
             ship_pos_y -= SHIP_VELOCITY
-            ship_animation()
 
     if keys[pygame.K_DOWN] and ship_pos_y < (1000 - 40):
         if keys[pygame.K_LSHIFT]:
             ship_pos_y += SHIP_VELOCITY / 3
-            ship_animation("down")
         else:
             ship_pos_y += SHIP_VELOCITY
-            ship_animation("down")
 
     redraw_game_window()
 
